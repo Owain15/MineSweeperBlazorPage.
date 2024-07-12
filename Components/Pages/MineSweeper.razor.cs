a82c88ad3b0dabe14cb42f5a13eb57f06859db4d
Owain15 <153022268+Owain15@users.noreply.github.com>
2024-07-12 01:08:00 +0100

    left and right click events done

--- Components/Pages/MineSweeper.razor.cs
+++ Components/Pages/MineSweeper.razor.cs
@@ -128,5 +128,46 @@ internal class MineField
 		}
 		return adjacentMines;
 	}
+	private void CascadeUnHiddenCell(int x, int y)
+	{
+		for (int i = 0; i < 8; i++)
+		{
+			var adjacentCellX = x + AdjacentCellValues[i, 0];
+			var adjacentCellY = y + AdjacentCellValues[i, 1];
+
+			if (adjacentCellX >= 0 && adjacentCellX < cellData.GetLength(0)
+				&& adjacentCellY >= 0 && adjacentCellY < cellData.GetLength(1) 
+				&&cellData[adjacentCellX, adjacentCellY].State == CellState.Hidden)
+			{ 
+				cellData[adjacentCellX, adjacentCellY].State = CellState.UnHidden; 
+			    if(cellData[adjacentCellX, adjacentCellY].AdjacentMineCount==0)
+				{ CascadeUnHiddenCell(adjacentCellX, adjacentCellY); }
+			}
+
+		}
+	}
+
+	public void HandleLeftClickEvent(int x, int y)
+	{
+		if (cellData[x,y].State == CellState.Hidden)
+		{ cellData[x, y].State = CellState.UnHidden; }
+
+		if (cellData[x,y].DoesCellContainsAMine && cellData[x,y].State == CellState.UnHidden)
+		{ IsGameStillRunning = false; }
+
+		if(cellData[x,y].State == CellState.UnHidden && cellData[x,y].AdjacentMineCount == 0 )
+		{ CascadeUnHiddenCell(x, y); }
+	}
+	public void HandleRightClickEvent(int x, int y)
+	{
+		if (cellData[x,y].State == CellState.Hidden)
+		{ cellData[x, y].State = CellState.Flagged; }
+		
+		else if (cellData[x, y].State == CellState.Flagged)
+		{ cellData[x, y].State = CellState.Hidden; }
+
+	}
+	public void HandleLeftClickHoldEvent(int x, int y) { }
+	public void HandleLeftClickHoldUpEvent(int x, int y) { }
 
 }
